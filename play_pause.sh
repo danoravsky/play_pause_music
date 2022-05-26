d08971dc7dfabd3ece4f074b91075d1a069a8fb0
Daniel Oravsky <orad01@gmail.com>
2022-05-26 07:32:25 +0200

    adujsted timing

--- play_pause.sh
+++ play_pause.sh
@@ -2,7 +2,10 @@ while true
 do
     if [[ "$(pmset -g | grep ' sleep')" == *"Safari"* ]];
         then osascript -e 'tell application "Music" to pause';
-        else sleep 0.5 && osascript -e 'tell application "Music" to play'; 
+        else sleep 2 && if [[ "$(pmset -g | grep ' sleep')" == *"Safari"* ]];
+            then osascript -e 'tell application "Music" to pause';
+            else osascript -e 'tell application "Music" to play'; 
+        fi; 
     fi
-    sleep 0.5
+    sleep 0.3
 done
